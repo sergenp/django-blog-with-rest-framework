ac9974092ce37858e67db1b26ffd44f96f62e1db
sergenp <sergen12343@gmail.com>
2020-07-21 12:09:22 +0300

    link views with url add views to frontend add navbar.html

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Ryn',
+        'title': 'Dummy Post',
+        'content': 'This is a Dummy Post',
+        'date_posted': '7/15/2020'
+    }
+]
+
 def home(request):
-    return render(request, "home.html")
+    context = {
+            'posts': posts
+        }
+    return render(request, 'home.html', context)
+
+
+def about(request):
+     return render(request, 'about.html', {'title': 'About'})
